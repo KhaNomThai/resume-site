38e2a7cc35f89d71a314c3e871825c2b23566ec3
KhaNomThai <kulachart2014@gmail.com>
2025-11-08 01:35:09 +0700

    fix profile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { profile } from "../data";
-import profileImg from "./assets/profile.jpg";
+import profileImg from "../assets/profile.jpg";
 
 export default function Home() {
   return (
